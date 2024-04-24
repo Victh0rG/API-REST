3deeb0a459e986f8a225883b02844d7bee1eaf9d
Victh0rG <105661120+Victh0rG@users.noreply.github.com>
2024-04-24 14:37:07 -0300

    Create/upadete/delete people.py
    criado as funções de criar, atualizar e deletar people

--- people.py
+++ people.py
@@ -28,3 +28,42 @@ PEOPLE = {
 def read_all():
     return list(PEOPLE.values())
 
+def creat_person(person):
+  lname = person.get('lname')
+  fname = person.get('fname')
+
+  if lname and lname not in PEOPLE:
+    PEOPLE[lname] = {
+        "lname": lname,
+        "fname": fname,
+        "timestamp": get_timestamp()
+    }
+    return PEOPLE[lname], 201
+  else:
+    abort(
+    406,
+    f"Person with last name {lname} already exists",
+    )
+
+def update(lname, person):
+  if lname in PEOPLE:
+    PEOPLE[lname]['fname'] = person.get('fname', PEOPLE[lname]['fname'])
+    PEOPLE[lname]['timestamp'] = get_timestamp()
+    return PEOPLE[lname], 200
+  else:
+    abort(
+      404,
+      f"Person with last name {lname} not found"
+      )
+
+def delete(lname):
+  if lname in PEOPLE:
+    del PEOPLE[lname]
+    return make_response(
+      f"{lname} successfully deleted", 200
+    )
+  else:
+    abort(
+     404,
+      f"Person with last name {lname} not found"
+      )
