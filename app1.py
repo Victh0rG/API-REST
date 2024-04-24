5dab42a7cb3e7d9590909905c9b5151a1179b60b
Victh0rG <105661120+Victh0rG@users.noreply.github.com>
2024-04-24 14:40:29 -0300

    conectando app com o yml app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,17 @@
 #   app.py
 
-from flask import Flask, render_template
+from flask import render_template
+import connexion
 
-app = Flask(__name__)
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
 
+aplication = app.app
 @app.route('/')
 def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
+    import uvicorn
+    uvicorn.run("main:application", host="0.0.0.0", port=8000, reload=True)
